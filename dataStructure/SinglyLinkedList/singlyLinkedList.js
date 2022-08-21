56133ede9b0b34f1bd61a1121f5937526b09ecd4
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-08-21 20:18:14 +0900

    push 메소드

---
+++ dataStructure/SinglyLinkedList/singlyLinkedList.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// let first = new Node("HI");
+// first.next = new Node("there");
+// first.next.next = new Node("here");
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  //메소드 추가
+  push(val) {
+    //주어진 값을 받아들인 후에 그것을 이용해서 새로운 노드 생성
+    //head가 없는 경우 리스트가 비어있다는 것을 뜻함
+    //리스트가 비어있지 않으면 마지막 노드의 next를 새롭게 생성된 노드를 가리키도록 하기
+    // 리스트의 길이도 증가시켜야 함
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode; //tail이 가리키고 있는 마지막 노드의 next가 새로운 노드를 가리키도록 함
+      this.tail = newNode; //list의 tail이 새로운 노드를 가리키도록
+    }
+    this.length++;
+    console.log("this", this);
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+// list.push("HEllo");
+// list.push("Bye");
+
+console.log(list);
