99f59d9cd9cf425663f262b999ee8f59ee487b03
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-02 18:34:19 +0900

    doubly linkedlist push 메소드

---
+++ dataStructure/DoublyLinkedList/doublyLinkedList_push.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    //1. 추가할 노드 생성
+    //2. head가 null이거나 길이가 0인지 확인
+    //3. 그렇다면 head,tail 모두 다 새로 만든 노드로 설정되어야 함
+    //4. 그리고 새로 만든 노드의 prev 프로퍼티를 이전 tail로 설정
+    //5. 그리고 tail 프로퍼티를 이제 가장 끝에 있게 된 새로운 노드로 이어준다.
+    //6. 길이를 1 늘리고 전체 리스트 출력
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
