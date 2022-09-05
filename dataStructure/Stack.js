f8a5af21efa86acb94a9ae12080c3def0366c619
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-05 21:47:53 +0900

    stack push,pop

---
+++ dataStructure/Stack.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.first === undefined) {
+      //스택에 노드가 하나도 없으면 새로운 노드가 first이자 last가 되도록 설정
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (this.first === undefined) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
