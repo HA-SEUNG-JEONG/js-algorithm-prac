ded3185d5276f2ba1f0d1ae3fe11dd73c1a31121
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-08 13:20:39 +0900

    insert 메소드

--- dataStructure/binarySearchTree/BST_Classes.js
+++ dataStructure/binarySearchTree/BST_Classes.js
@@ -10,10 +10,41 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
+//      10
+//   5     13
+// 2  7  11  16
+
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
