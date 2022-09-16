5668a9494bd09e8948588eaf546272778bf02bd8
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-16 22:00:59 +0900

    DFS - 전위 순회

---
+++ dataStructure/DFS/Depth_First_Tree.js
@@ -0,0 +1,102 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    let data = [];
+    let current = this.root; //사용자가 시작하기 원하는 노드가 있을수도 있음. 항상 루트부터 순회한다는 보장이 없다.
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left); //왼쪽 노드가 존재하면 왼쪽에 대해서 traverse 함수를 재귀 호출
+      if (node.right) traverse(node.right); //오른쪽 노드가 존재하면 오른쪽에 대해서 traverse 함수를 재귀 호출
+    }
+    traverse(current); //root에 traverse 함수 호출
+    return data;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.DFSPreOrder();
