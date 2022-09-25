b7cbfaeac62ad3327daadd3fd93f58b797be1cb8
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-25 10:02:18 +0900

    Edge 케이스 코드 수정

--- dataStructure/graph/vertexAndEdge.js
+++ dataStructure/graph/vertexAndEdge.js
@@ -24,30 +24,23 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+  contains(vertex) {
+    return Object.keys(this.adjacencyList).includes(vertex) ? true : false;
+  }
   removeEdge(vertex1, vertex2) {
     //각 정점들의 값에서 상대 정점만 제외해서 반환
 
     //addVertex 했을 때 그래프에 포함된 정점이 아니면 false 리턴
-    if (
-      !this.adjacencyList[vertex2].includes(vertex1) ||
-      !this.adjacencyList[vertex1].includes(vertex2)
-    )
-      return false;
+    if (!this.contains(vertex1) || !this.contains(vertex2)) return false;
 
-    // this.adjacencyList[vertex1] = filter(
-    //   (vertex) => vertex !== vertex2,
-    //   this.adjacencyList[vertex1]
-    // );
-
-    // this.adjacencyList[vertex2] = filter(
-    //   (vertex) => vertex !== vertex1,
-    //   this.adjacencyList[vertex2]
-    // );
-    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-      (vertex) => vertex !== vertex2
+    this.adjacencyList[vertex1] = filter(
+      (vertex) => vertex !== vertex2,
+      this.adjacencyList[vertex1]
     );
-    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-      (vertex) => vertex !== vertex1
+
+    this.adjacencyList[vertex2] = filter(
+      (vertex) => vertex !== vertex1,
+      this.adjacencyList[vertex2]
     );
   }
 }
@@ -58,5 +51,5 @@ graph.addVertex("미국");
 graph.addVertex("홍콩");
 graph.addVertex("한국");
 graph.addEdge("도쿄", "한국");
-graph.removeEdge("도쿄", "홍콩");
+graph.removeEdge("도쿄", "영국");
 console.log(graph);
