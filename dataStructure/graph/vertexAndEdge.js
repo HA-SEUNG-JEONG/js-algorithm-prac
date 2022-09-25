05445a3102ac2a6ac34e569325bb83d367c60949
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-25 09:57:37 +0900

    Edge 케이스 추가

--- dataStructure/graph/vertexAndEdge.js
+++ dataStructure/graph/vertexAndEdge.js
@@ -25,13 +25,29 @@ class Graph {
     this.adjacencyList[vertex2].push(vertex1);
   }
   removeEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1] = filter(
-      (vertex) => vertex !== vertex2,
-      this.adjacencyList[vertex1]
+    //각 정점들의 값에서 상대 정점만 제외해서 반환
+
+    //addVertex 했을 때 그래프에 포함된 정점이 아니면 false 리턴
+    if (
+      !this.adjacencyList[vertex2].includes(vertex1) ||
+      !this.adjacencyList[vertex1].includes(vertex2)
+    )
+      return false;
+
+    // this.adjacencyList[vertex1] = filter(
+    //   (vertex) => vertex !== vertex2,
+    //   this.adjacencyList[vertex1]
+    // );
+
+    // this.adjacencyList[vertex2] = filter(
+    //   (vertex) => vertex !== vertex1,
+    //   this.adjacencyList[vertex2]
+    // );
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
     );
-    this.adjacencyList[vertex2] = filter(
-      (vertex) => vertex !== vertex1,
-      this.adjacencyList[vertex2]
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
     );
   }
 }
@@ -42,5 +58,5 @@ graph.addVertex("미국");
 graph.addVertex("홍콩");
 graph.addVertex("한국");
 graph.addEdge("도쿄", "한국");
-graph.removeEdge("도쿄", "한국");
+graph.removeEdge("도쿄", "홍콩");
 console.log(graph);
