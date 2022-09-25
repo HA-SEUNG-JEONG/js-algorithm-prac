c500054d740142cc3d38ef61e0a2b04a5bf92bf9
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-09-25 09:13:57 +0900

    addEdge

--- dataStructure/graph/addVertex.js
+++ dataStructure/graph/addVertex.js
@@ -1,9 +1,26 @@
 class Graph {
   constructor() {
-    this.adjacencyyList = {};
+    this.adjacencyList = {};
   }
   addVertex(vertex) {
     //해당 정점에 값이 없다면 빈 배열로 처리
-    if (!this.adjacencyyList[vertex]) this.adjacencyyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  //간선 추가 메소드
+  //두 개의 정점을 명시해야 함
+  //이 메소드는 adjacencyList에서 vertex1의 키를 찾아서 vertex2를 그 배열에 넣어줘야 함
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) this.addVertex(vertex1);
+    if (!this.adjacencyList[vertex2]) this.addVertex(vertex1);
+    //undirected 처리
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
+
+let graph = new Graph();
+graph.addVertex("도쿄");
+graph.addVertex("미국");
+graph.addVertex("한국");
+graph.addEdge("도쿄", "한국");
+console.log(graph);
